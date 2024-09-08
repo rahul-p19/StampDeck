a0ce3c268dbbaf10b60d9e316f932c55d030247f
Swapnaneel26R <neel26ray@gmail.com>
2024-09-08 19:25:34 +0530

    pushing changes in about and contact section

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,7 +1,33 @@
-function About() {
-  return (
-    <div className="h-screen" id="about">About</div>
-  )
+import aboutImg from "../assets/stock.jpg";
+
+   function About() {
+     return ( 
+       <div  id="about" className="h-screen">
+    
+   
+           <h1 className="my-20 text-center pt-10 pb-0 text-6xl font-inter text-accent font-bold">ABOUT US</h1>
+           <div className="flex flex-wrap">
+             <div className="w-full lg:w-1/2 lg:p-1 ">
+             <div className="flex items-center justify-center">
+               <img className="rounded-2xl "src={ aboutImg } alt="image of a card" />
+
+             </div>
+           </div>
+           <div className="w-full lg:w-1/2">
+             <div className="flex justify-center lg:justify-center">
+               <p className="my-2 leading-9 max-w-xl px-3  flex  text-primary text-xl font-inter font-semibold" >
+                 StampDeck is more than just a website.<br></br>It is a vibrant online community dedicated to fostering a love for philately.
+                 Be a part of a nationwide network of collectors, buy, sell, and trade stamps with ease, and discover a wealth of knowledge on our user-friendly platform.<br></br> By promoting philately, we contribute to the preservation of cultural heritage while also minimizing our carbon footprint. <br></br>Experience the joy of philately in a convenient, connected, and environmentally friendly way.
+                 </p>
+             </div>
+           </div>
+        </div>
+         </div>
+ 
+         
+       
+ );
 }
 
+ 
 export default About
--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,10 +1,71 @@
-function Contact() {
+ 
+
+  function Contact() {
+ 
+
   return (
-    <>
-    <div className="h-screen" id="contact">
-        <h1>Contact Us</h1>
+     <div className="h-screen max-w-full bg-background"  id="contact">
+      
+      
+
+      <div className="w-full  grid place-items-center">
+     
+        <div className="mt-6 bg-secondary rounded-xl place-items-center w-3/7">
+          <div className="p-10">
+
+            <form action="#" method="POST">
+              <div className="grid grid-cols-1 p-10">
+   <div className="text-center pt-0 pb-10 ">
+        <h2 className="text-6xl font-bold   leading-tight text-primary">CONTACT</h2>
+
+      </div>
+
+                <div className="name">
+                  <div className="mt-2.5 ">
+                    <input type="text" name="" id="" placeholder="Your Name"
+                      className="w-full px-4 py-4 text-text placeholder-text border 
+                    rounded-md foucus:border-secondary-600" rows="2"/>
+                    
+                  </div>
+
+                  
+                </div>
+
+                <div className="email">
+                  <div className="mt-2.5 ">
+                    <input type="text" name="" id="" placeholder="Your Email ID"
+                      className="w-full px-4 py-4 text-text placeholder-text border 
+                    rounded-md foucus:border-secondary-600" rows="2"/>
+                    
+                  </div>
+
+                  <div className="feedback">
+                    <div className="mt-2.5">
+                      <textarea name="" placeholder="Send a Message" className="w-full px-4 py-4
+                       text-text placeholder-text border 
+                    rounded-md foucus:border-secondary-600" rows="4" >
+
+                      </textarea>
+                    </div>
+                  </div>
+
+                  <div className="sm:col-span-2">
+                    <button type="submit" className="text-xl w-3/4 p-4 mt-2 font-semibold text-white
+                    bg-accent rounded-xl">
+                      Submit
+                    </button>
+                  </div>
+                  
+                </div>
+              </div>
+
+            </form>
+
+          </div>
+       </div>
+
+      </div>
     </div>
-    </>
   )
 }
 
